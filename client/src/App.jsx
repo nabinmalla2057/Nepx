b81e1d4ab89a2d7e7e93a2270e7dfe684d1eeac8
Nabin Malla <nabin.malla@patancollege.edu.np>
2023-10-18 11:46:21 +0545

    saved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,16 +25,33 @@ const router = createBrowserRouter([
 export default function App() {
   return (
     <>
-      <body className="d-flex flex-column h-100">
-        <ThemeSelector />
+      <div className="d-flex flex-column h-100vh">
+        {/* <ThemeSelector /> */}
         <Navbar />
         <main className="flex-shrink-0">
-          <div className="container"></div>
-          <RouterProvider router={router} />
+          {/* <div className="container">
+            <RouterProvider router={router} />
+          </div> */}
+          <div className="container">
+            <h1 className="mt-5">Sticky footer with fixed navbar</h1>
+            <p className="lead">
+              Pin a footer to the bottom of the viewport in desktop browsers
+              with this custom HTML and CSS. A fixed navbar has been added with{" "}
+              <code className="small">padding-top: 60px;</code> on the{" "}
+              <code className="small">main &gt; .container</code>.
+            </p>
+            <p>
+              Back to{" "}
+              <a href="/docs/5.3/examples/sticky-footer/">
+                the default sticky footer
+              </a>{" "}
+              minus the navbar.
+              <RouterProvider router={router} />
+            </p>
+          </div>
         </main>
-        <RouterProvider router={router} />
         <Footer />
-      </body>
+      </div>
     </>
   );
 }
